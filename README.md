6505070c927921a8c6e04baba2f355856469a256
Joel Medeiros <joel.joao.medeiros@gmail.com>
2022-09-06 17:08:30 -0300

    modified:   README.md 	new file:   resolucaoExercicios/Lista 1/ex1.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Exercicios Disciplina de Algoritmos e Programação
+**Disciplina minstrada por:** Cristian Checinel  
+**Ofertada na:** Universidade Federal de Santa Catarina  
 
---
+++ resolucaoExercicios/Lista 1/ex1.py
@@ -0,0 +1,10 @@
+# Resolução do exercicio 1 da lista 1
+
+from datetime import datetime
+
+diaNascimento = input("Digite o dia em que você nasceu: ")
+mesNascimento = input("Digite o mês do seu nascimento: ")
+anoNascimento = input("Digite o ano em que nasceu: ")
+
+
+
