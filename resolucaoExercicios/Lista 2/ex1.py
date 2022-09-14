f124c136660adcc9c0fba98d524d7a8d154b66fb
Joel Medeiros <joel.joao.medeiros@gmail.com>
2022-09-14 16:29:19 -0300

    modified:   2. estruturaCondicao.md 	new file:   resolucaoExercicios/Lista 2/ex1.py

---
+++ resolucaoExercicios/Lista 2/ex1.py
@@ -0,0 +1,19 @@
+#Resolução do exercicio 1 da lista 2
+entrada = input("Digite o nome de usuario: ")
+
+if entrada == "Anakin":
+    print(f"Oi {entrada}, seja bem-vindo!")
+
+elif entrada == "Darth Vader":
+    print(f"Oi {entrada}, seja bem-vindo!")
+
+elif entrada == "Luke":
+    print(f"Oi {entrada}, seja bem-vindo!")
+
+elif entrada == "Leia":
+    print(f"Oi {entrada}, seja bem-vindo!")
+
+elif entrada == "Darth Sidius":
+    print(f"Oi {entrada}, seja bem-vindo!")
+else:
+    print(f"Usuario {entrada} não autorizado!")
