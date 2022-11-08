53982208ff00d1e08a17bb26286da151f98273de
Joel Medeiros <joel.joao.medeiros@gmail.com>
2022-11-08 14:35:35 -0300

    new file:   "6. estruturasRepeti\303\247\303\243oAninhadasSeries.md" 	modified:   resolucaoExercicios/Lista 3/ex3.py 	modified:   resolucaoExercicios/Lista 6/ex1.py

--- resolucaoExercicios/Lista 3/ex3.py
+++ resolucaoExercicios/Lista 3/ex3.py
@@ -1,1 +1,14 @@
 #Resolução do exercicio 3 da lista 3
+
+valor1 = input()
+valor2 = input()
+valor3 = input()
+valor4 = input()
+
+if valor1 == valor2 == valor3 == valor4:
+    print('temos um valor distinto')
+
+elif valor1 == valor2 == valor3 != valor4:
+    print('temos 2 valores distintos')
+
+# A fazer
--- resolucaoExercicios/Lista 6/ex1.py
+++ resolucaoExercicios/Lista 6/ex1.py
@@ -1,1 +1,19 @@
 # Resolução do exercicio 1 da lista 6
+
+n = int(input())
+
+anterior = 1
+atual = None
+for numero in range(1, n):
+    
+    if atual == None:
+        atual = 1
+    else:
+        temp = atual
+        atual = atual +anterior
+        anterior = temp
+    
+    #Esse print serve para visualizar os passos de fibonacci
+    #print(f'Atual {atual}, Anterior {anterior}')
+
+print(atual)
